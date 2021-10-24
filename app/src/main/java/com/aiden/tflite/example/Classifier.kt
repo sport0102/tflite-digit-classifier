69b59f56a24de957011141b82f2f8b46a650ceac
Sun min Lee <sunmin.lee@pbagel.com>
2021-10-24 12:35:24 +0900

    5.4 입력 이미지 전처리

--- app/src/main/java/com/aiden/tflite/example/Classifier.kt
+++ app/src/main/java/com/aiden/tflite/example/Classifier.kt
@@ -1,6 +1,7 @@
 package com.aiden.tflite.example
 
 import android.content.res.AssetManager
+import android.graphics.Bitmap
 import org.tensorflow.lite.Interpreter
 import java.io.FileInputStream
 import java.nio.ByteBuffer
@@ -8,12 +9,16 @@ import java.nio.ByteOrder
 import java.nio.channels.FileChannel
 
 class Classifier(private val assetManager: AssetManager, private val modelName: String) {
-    lateinit var interpreter: Interpreter
+    private lateinit var interpreter: Interpreter
+    private var modelInputChannel = 0
+    private var modelInputWidth = 0
+    private var modelInputHeight = 0
 
     fun init() {
         val model = loadModelFile()
         model.order(ByteOrder.nativeOrder())
         interpreter = Interpreter(model)
+        initModelShape()
     }
 
     private fun loadModelFile(): ByteBuffer {
@@ -23,6 +28,37 @@ class Classifier(private val assetManager: AssetManager, private val modelName: 
         val startOffset = assetFileDescriptor.startOffset
         val declaredLength = assetFileDescriptor.declaredLength
         return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
+    }
+
+    private fun initModelShape() {
+        val inputTensor = interpreter.getInputTensor(0)
+        val inputShape = inputTensor.shape()
+        modelInputChannel = inputShape[0]
+        modelInputWidth = inputShape[1]
+        modelInputHeight = inputShape[2]
+    }
+
+    private fun resizeBitmap(bitmap: Bitmap) =
+        Bitmap.createScaledBitmap(bitmap, modelInputWidth, modelInputHeight, false)
+
+    private fun convertBitmapGrayByteBuffer(bitmap: Bitmap): ByteBuffer {
+        val byteBuffer = ByteBuffer.allocateDirect(bitmap.byteCount)
+        byteBuffer.order(ByteOrder.nativeOrder())
+
+        val pixels = IntArray(bitmap.width * bitmap.height)
+        bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+
+        pixels.forEach { pixel ->
+            val r = pixel shr 16 and 0xFF
+            val g = pixel shr 8 and 0xFF
+            val b = pixel and 0xFF
+
+            val avgPixelValue = (r + g + b) / 3.0f
+            val normalizedPixelValue = avgPixelValue / 255.0f
+
+            byteBuffer.putFloat(normalizedPixelValue)
+        }
+        return byteBuffer
     }
 
     companion object {
