1f2aad718119fca3fd935c48b5cc93687fc3db62
Sun min Lee <sunmin.lee@pbagel.com>
2021-10-24 11:29:01 +0900

    5.3 TFLite 모델 로드

--- app/src/main/java/com/aiden/tflite/example/DrawActivity.kt
+++ app/src/main/java/com/aiden/tflite/example/DrawActivity.kt
@@ -2,16 +2,20 @@ package com.aiden.tflite.example
 
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.aiden.tflite.example.databinding.ActivityDrawBinding
+import java.io.IOException
 
 class DrawActivity : AppCompatActivity() {
     private val binding by lazy { ActivityDrawBinding.inflate(layoutInflater, null, false) }
+    private lateinit var classifier: Classifier
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initCanvas()
+        initClassifier()
     }
 
     private fun initCanvas() {
@@ -27,6 +31,15 @@ class DrawActivity : AppCompatActivity() {
             btnClear.setOnClickListener {
                 drawView.clearCanvas()
             }
+        }
+    }
+
+    private fun initClassifier() {
+        classifier = Classifier(assets, Classifier.DIGIT_CLASSIFIER)
+        try {
+            classifier.init()
+        } catch (exception: IOException) {
+            Toast.makeText(this@DrawActivity, "classifier IOException", Toast.LENGTH_SHORT).show()
         }
     }
 }
